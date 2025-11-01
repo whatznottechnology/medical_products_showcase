dc54bf57c6af71240f8372c71f5e49fd02e79f1b
Whatznot Technology <whatznottechnology@gmail.com>
2025-11-02 02:09:26 +0530

    Remove icon from product details price section

--- product-details.php
+++ product-details.php
@@ -169,9 +169,6 @@ echo getNavigation();
                                     <?php endif; ?>
                                 </p>
                             </div>
-                            <svg class="w-10 h-10 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                            </svg>
                         </div>
                         <p class="text-xs text-gray-600 mt-2">* Prices may vary based on quantity and customization</p>
                     </div>
